6aeb4fa828413616a6e958e3698e5431ba98c9dd
Vineeth Es <vineeth@datamateindia.com>
2024-08-08 10:15:12 +0530

    latest changes added

--- mail.js
+++ mail.js
@@ -27,5 +27,34 @@ function sendEmail(){
    
 }
 
+function modalSendEmail(){
+    console.log('hi');
+    var param={
+        name: document.getElementById('modalname').value,
+        email: document.getElementById('modalemail').value,
+        mobile: document.getElementById('modalmobile').value,
+        service: document.getElementById('modalservice').value,
+        location: document.getElementById('modallocation').value,
+        message: document.getElementById('modalmessage').value,
+    }
+
+    const serviceId='service_50r1zrj'
+    const templateId='template_bmvihbq'
+
+    emailjs.send(serviceId,templateId,param).then(
+        (res)=>{
+            console.log(res);
+            document.getElementById('modalname').value =''
+        document.getElementById('modalemail').value=''
+        document.getElementById('modalmobile').value=''
+        document.getElementById('modalservice').value=''
+        document.getElementById('modallocation').value=''
+        document.getElementById('modalmessage').value=''
+            alert("successfully submit details")
+        }
+    ).catch(err=>{console.log(err);})
+   
+}
+
 
  
